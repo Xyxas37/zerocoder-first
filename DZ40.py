5cacd4434dcb3b04def776ccbda38da56990bbfe
Xyxas37 <xyxas37@gmail.com>
2025-03-08 09:54:47 +0300

    dz40a
    dz40a

--- DZ40.py
+++ DZ40.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    current_time = datetime.now().strftime("%d %B %Y, %H:%M:%S")
+    return render_template("index.html", current_time=current_time)
 
 @app.route("/blog")
 def blog():
